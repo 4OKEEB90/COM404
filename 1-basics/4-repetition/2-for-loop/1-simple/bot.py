fa140909e95294d4b0f9f1ddc4c2934f69e9eea3
4OKEEB90 <4okeeb90@solent.ac.uk>
2019-10-08 10:04:42 +0000

    Added code to demonstrate the use of a simple for loop.

---
+++ 1-basics/4-repetition/2-for-loop/1-simple/bot.py
@@ -0,0 +1,7 @@
+for count in range(0, 3, 1):
+    print("      __ ")
+    print("     /  \_ ") 
+    print("    /^    \ ")
+    print("   / ^     \_ ")
+    print(" _/ ^ ^     ^\ ")
+    print("/  ^     ^    \ ")
