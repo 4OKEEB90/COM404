c1852e06ccd64224a66f79b4c5ec6590f03120f6
4OKEEB90 <4okeeb90@solent.ac.uk>
2019-10-10 16:24:54 +0000

    test

---
+++ 1-basics/4-repetition/3-nested-loop/1-nested/bot.py
@@ -0,0 +1,15 @@
+print("How many rows and columns would you like?")
+row = int(input("Rows: "))
+
+print("How many rows and columns would you like?")
+col = int(input())
+
+print("Here I go:")
+
+for count in range (0,row,1):
+    for count in range (0,col,1):
+        print(+":-)"*col[count])
+
+print("Done!")
+
+
