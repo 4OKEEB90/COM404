4a97967d802acd5ffed51e3a90fb9c14bd816829
4OKEEB90 <4okeeb90@solent.ac.uk>
2019-10-11 14:26:39 +0000

    Added code to demonstrate the use of one loop nested is another.

--- 1-basics/4-repetition/3-nested-loop/1-nested/bot.py
+++ 1-basics/4-repetition/3-nested-loop/1-nested/bot.py
@@ -4,12 +4,13 @@ row = int(input("Rows: "))
 print("How many rows and columns would you like?")
 col = int(input())
 
-print("Here I go:")
+print("Here I go:\n")
 
 for count in range (0,row,1):
     for count in range (0,col,1):
-        print(+":-)"*col[count])
+        print(":-)", end="")
+    print("")
 
-print("Done!")
+print("\nDone!")
 
 
