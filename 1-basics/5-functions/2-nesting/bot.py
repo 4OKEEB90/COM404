a96d6bf8a135b05bc7c66098d91b1f5d5ad33d1d
4OKEEB90 <4okeeb90@solent.ac.uk>
2019-10-15 08:58:30 +0000

    Added code to demonstrate the use of a user-defined function with a parameter.

---
+++ 1-basics/5-functions/2-nesting/bot.py
@@ -0,0 +1,9 @@
+def identify():
+    print("What lies before us?")
+    see = str(input())
+    if see == "a large boulder":
+        print("\nIt's time to run!")
+    else:
+        print("\nwe will be fine.")
+
+identify()
