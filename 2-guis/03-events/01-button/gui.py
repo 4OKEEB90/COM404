03d0e5e263f8cec953b7903d066482bdcf4a7cba
Ben O'Keeffe <4okeeb90@solent.ac.uk>
2019-11-19 11:34:37 +0000

    2 done

--- 2-guis/03-events/01-button/gui.py
+++ 2-guis/03-events/01-button/gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 class Gui(Tk):
     
@@ -11,7 +12,7 @@ class Gui(Tk):
         # add components
         self.__add_heading_label()
         self.__add_instruction_label()
-        self.__add_tickets_entry()
+        self.__add_ticket_entry()
         self.__add_buy_button()
         
     def __add_heading_label(self):
@@ -26,7 +27,7 @@ class Gui(Tk):
     # style
         self.instruction_label.configure(font="Arial 10", text="How many tickets are needed?")
         
-    def __add_tickets_entry(self):
+    def __add_ticket_entry(self):
         self.ticket_entry = Entry()
         self.ticket_entry.grid(row=3,column=0)
         
@@ -35,3 +36,13 @@ class Gui(Tk):
         self.buy_button.grid(row=4,column=0)
     #style
         self.buy_button.configure(font="Arial 10", text="Buy")
+    # events
+        self.buy_button.bind("<ButtonRelease-1>", self.__buy_button_clicked)
+        
+    def __buy_button_clicked(self, event):
+        if (int(self.ticket_entry.get()) == 1):
+            messagebox.showinfo("Purchased!", "You have purchased 1 ticket!")
+        elif (int(self.ticket_entry.get()) > 1):
+            messagebox.showinfo("Purchased!", "You have purchased "+str(self.ticket_entry.get())+" tickets!")
+        else:
+            messagebox.showinfo("Error!", "You have entered an invalid number of tickets!")
